8eb1e70b2ae31b4b749147f25091b873fec8a55c
Zune <jjgransbury@hotmail.com>
2019-10-08 00:30:53 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useRef } from 'react'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const url = 'https://api.spotify.com/v1'
+
+const getData = async endpoint => await (await fetch(url + endpoint)).json()
+
+const fetchData = async (current, endpoint) => {
+  const results = fetchData(
+    getData(current && current.next ? current.next : url + endpoint)
+  )
+  return [...current.items, ...results]
 }
 
-export default App;
+function App() {
+  const followedArtists = useRef([])
+  useEffect(() => {
+    fetchData().then(result => (followedArtists.current = result))
+  }, [followedArtists])
+
+  return (
+    <div className="App">
+      <ul>
+        {followedArtists.current.map(d => (
+          <li key={d.id}>{d}</li>
+        ))}
+      </ul>
+    </div>
+  )
+}
+
+export default App
